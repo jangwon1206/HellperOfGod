fc2ecdfaae88c80653010d9038504af90e453f89
jangwon1206 <66582923+jangwon1206@users.noreply.github.com>
2021-01-22 00:42:44 +0900

    2021_01_21
    기본공격 적용
    battle상태시 움직임 적용예정

--- CameraController.cs
+++ CameraController.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float Camera_StartMouseControlPos_X = 0f;
-    private float Camera_StartMouseControlPos_Y = 0f;
+    private float camera_StartMouseControlPos_X = 0f;
+    private float camera_StartMouseControlPos_Y = 0f;
 
-    private float Camera_ControlRotation_X = 0f;
-    private float Camera_ControlRotation_Y = 0f;
+    private float camera_ControlRotation_X = 0f;
+    private float camera_ControlRotation_Y = 0f;
 
-    private float Camera_ResultRotation_X = 0f;
-    private float Camera_ResultRotation_Y = 0f;
+    private float camera_ResultRotation_X = 0f;
+    private float camera_ResultRotation_Y = 0f;
 
     public Camera _Camera = null;
 
     public GameObject _CameraPos = null;
 
-    private RaycastHit CameraPosRay;
+    private RaycastHit cameraPosRay;
 
-    private float CameraMaxDistance = 0f;
+    private float cameraMaxDistance = 0f;
 
-    private float CameraDistance = 0f;
+    private float cameraDistance = 0f;
 
     public int layerMask_Map = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        CameraMaxDistance = -18f;
-        CameraDistance = CameraMaxDistance;
+        cameraMaxDistance = -18f;
+        cameraDistance = cameraMaxDistance;
         layerMask_Map = 1 << LayerMask.NameToLayer("Map");
     }
 
@@ -38,79 +38,79 @@ public class CameraController : MonoBehaviour
     {
         this.transform.position = _CameraPos.transform.position;
         //작업용 카메라위치 RAY
-        Debug.DrawRay(this.transform.position, this.transform.forward * CameraDistance, Color.red, 0.3f);
+        Debug.DrawRay(this.transform.position, this.transform.forward * cameraDistance, Color.red, 0.3f);
 
         //캐릭터 비추는 카메라의 위치를 조정
-        if (Physics.Raycast(this.transform.position, -this.transform.forward, out CameraPosRay, -CameraMaxDistance, layerMask_Map))
+        if (Physics.Raycast(this.transform.position, -this.transform.forward, out cameraPosRay, -cameraMaxDistance, layerMask_Map))
         {
             //Map오브젝트가 캐릭터를 가릴 경우
-            if(CameraPosRay.collider.CompareTag("Map"))
+            if(cameraPosRay.collider.CompareTag("Map"))
             {
-                CameraDistance = -Vector3.Distance(this.transform.position, CameraPosRay.point); //Ray에 닿은 거리 (카메라의 거리)
+                cameraDistance = -Vector3.Distance(this.transform.position, cameraPosRay.point); //Ray에 닿은 거리 (카메라의 거리)
             }
             //가리지 않을 경우
             else
             {
-                CameraDistance = CameraMaxDistance;  //카메라 최대거리
+                cameraDistance = cameraMaxDistance;  //카메라 최대거리
             }
 
         }
         //Ray가 닿지 않을 경우 ( 최대거리 )
         else
         {
-            CameraDistance = CameraMaxDistance;
+            cameraDistance = cameraMaxDistance;
         }
         
 
         //카메라의 위치
-        _Camera.transform.localPosition= new Vector3(0f, 0f, CameraDistance);
+        _Camera.transform.localPosition= new Vector3(0f, 0f, cameraDistance);
 
         //마우스 오른쪽 키다운시 눌렀던 화면 처음포지션확인
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            Camera_StartMouseControlPos_X = Input.mousePosition.x;
-            Camera_StartMouseControlPos_Y = Input.mousePosition.y;
+            camera_StartMouseControlPos_X = Input.mousePosition.x;
+            camera_StartMouseControlPos_Y = Input.mousePosition.y;
         }
 
         if (Input.GetKey(KeyCode.Mouse1))
         {
             //마우스 클릭하여 드래그시 카메라의 방향을 조정
-            Camera_ControlRotation_X = (Camera_StartMouseControlPos_X - Input.mousePosition.x) * 0.01f;
-            Camera_ControlRotation_Y = (Camera_StartMouseControlPos_Y - Input.mousePosition.y) * 0.01f;
+            camera_ControlRotation_X = (camera_StartMouseControlPos_X - Input.mousePosition.x) * 0.01f;
+            camera_ControlRotation_Y = (camera_StartMouseControlPos_Y - Input.mousePosition.y) * 0.01f;
 
-            if (Camera_ControlRotation_X >= 2f)
+            if (camera_ControlRotation_X >= 2f)
             {
-                Camera_ControlRotation_X = 2f;
+                camera_ControlRotation_X = 2f;
             }
-            if (Camera_ControlRotation_Y >= 2f)
+            if (camera_ControlRotation_Y >= 2f)
             {
-                Camera_ControlRotation_Y = 2f;
+                camera_ControlRotation_Y = 2f;
             }
 
-            Camera_ResultRotation_X = this.transform.eulerAngles.x + Camera_ControlRotation_Y;
-            Camera_ResultRotation_Y = this.transform.eulerAngles.y - Camera_ControlRotation_X;
+            camera_ResultRotation_X = this.transform.eulerAngles.x + camera_ControlRotation_Y;
+            camera_ResultRotation_Y = this.transform.eulerAngles.y - camera_ControlRotation_X;
 
            
 
-            this.transform.rotation = Quaternion.Euler(Camera_ResultRotation_X, Camera_ResultRotation_Y, 0f);
+            this.transform.rotation = Quaternion.Euler(camera_ResultRotation_X, camera_ResultRotation_Y, 0f);
 
         }
 
         //휠 밀면 카메라 가까워짐
         if (Input.GetAxis("Mouse ScrollWheel")> 0f)
         {
-            if (CameraMaxDistance < -3f)
+            if (cameraMaxDistance < -3f)
             {
-                CameraMaxDistance += 1f;
+                cameraMaxDistance += 1f;
             }
         }
 
         //휠 당기면 카메라 멀어짐
         if (Input.GetAxis("Mouse ScrollWheel")< 0f)
         {
-            if (CameraMaxDistance > -20f)
+            if (cameraMaxDistance > -20f)
             {
-                CameraMaxDistance -= 1f;
+                cameraMaxDistance -= 1f;
             }
             
         }
--- PlayerController.cs
+++ PlayerController.cs
@@ -4,171 +4,348 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    private Vector3 ForwardVec = Vector3.zero;  //forward가 될 Vector
+
+    private Vector3 forwardVec = Vector3.zero;  //forward가 될 Vector
 
     public Camera _Camera = null;
 
-    //움직임 제한을 위한 Ray를 쏠 위치
-    public GameObject _TileCheckPos = null;
+    public GameObject _Weapon = null;
 
-    public GameObject _CharacterCenterPos = null;
+    private string weapon_kind = "";
 
-    private Animator Ani;
+    public float weapon_Speed = 0f;
 
-    private bool Run = false;
+    private Animator _Ani;
 
-    private float MoveSpeed = 0f; //Player MoveSpeed
+    //버튼체크용
+    private bool run = false;
+    private bool attack = false;
+
+    private bool wait = false;
+    private bool battle = false;
+    private bool idleComebackCheck = false;
+    private bool combo = false;
+
+    public int attack_Combo = 0;
+    private bool comboTime = false;
+
+
+    //실제 달리기 체크용
+    public bool runing = false;
+
+    private UIMgr _UIMgr = null;
+
+    private float moveSpeed = 0f; //Player moveSpeed
 
     //입력에 따른 Forward에 쓰일 인수
-    private float UpButtonVec_X = 0f;
-    private float UpButtonVec_Z = 0f;
-    private float DownButtonVec_X = 0f;
-    private float DownButtonVec_Z = 0f;
-    private float RightButtonVec_X = 0f;
-    private float RightButtonVec_Z = 0f;
-    private float LeftButtonVec_X = 0f;
-    private float LeftButtonVec_Z = 0f;
+    private float upButtonVec_X = 0f;
+    private float upButtonVec_Z = 0f;
+    private float downButtonVec_X = 0f;
+    private float downButtonVec_Z = 0f;
+    private float rightButtonVec_X = 0f;
+    private float rightButtonVec_Z = 0f;
+    private float leftButtonVec_X = 0f;
+    private float leftButtonVec_Z = 0f;
 
     //입력을 확인하는 값
-    private int UpButton = 0;
-    private int DownButton = 0;
-    private int LeftButton = 0;
-    private int RightButton = 0;
+    private int upButton = 0;
+    private int downButton = 0;
+    private int leftButton = 0;
+    private int rightButton = 0;
 
     //버튼의 입력 총값
-    private int PushCount = 0;
+    private int pushCount = 0;
 
     //Forward 결과 인수
-    private float ForwardVec_X = 0;
-    private float ForwardVec_Z = 0;
+    private float forwardVec_X = 0;
+    private float forwardVec_Z = 0;
 
     //상수들
-    private const int ButtonDown = 1;   //버튼 PreesDown
-    private const int ButtonUp = 0;     //버튼 PreesUp
+    private const int buttonDown = 1;   //버튼 PreesDown
+    private const int buttonUp = 0;     //버튼 PreesUp
 
+    private const float weapon_TwoHandAxe = 1f;
+    private const float weapon_TwoHandSword = 2f;
+    private const float weapon_OneHandSword = 3f;
 
-   
+    private IEnumerator timer_AttackCombo;
+    private IEnumerator timer_BattleState_Off;
+
+    private enum State
+    {
+        NOMAL,
+        BATTLE,
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Ani = this.GetComponent<Animator>();
-        MoveSpeed = 1f;
+        _Ani = this.GetComponent<Animator>();
+        _UIMgr = FindObjectOfType<UIMgr>();
+
+        moveSpeed = 1f;
+        timer_AttackCombo = Timer_AttackCombo();
+        timer_BattleState_Off = Timer_BattleState_Off();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
+
         if (Input.GetKey(KeyCode.W))
         {
-            UpButton = ButtonDown;
-            UpButtonVec_X = _Camera.transform.forward.x;
-            UpButtonVec_Z = _Camera.transform.forward.z;
+            upButton = buttonDown;
+            upButtonVec_X = _Camera.transform.forward.x;
+            upButtonVec_Z = _Camera.transform.forward.z;
         }
         else if (Input.GetKeyUp(KeyCode.W))
         {
-            UpButton = ButtonUp;
-            UpButtonVec_X = 0f;
-            UpButtonVec_Z = 0f;
+            upButton = buttonUp;
+            upButtonVec_X = 0f;
+            upButtonVec_Z = 0f;
         }
 
 
         if (Input.GetKey(KeyCode.S))
         {
-            DownButton = ButtonDown;
-            DownButtonVec_X = -_Camera.transform.forward.x;
-            DownButtonVec_Z = -_Camera.transform.forward.z;
+            downButton = buttonDown;
+            downButtonVec_X = -_Camera.transform.forward.x;
+            downButtonVec_Z = -_Camera.transform.forward.z;
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
-            DownButton = ButtonUp;
-            DownButtonVec_X = 0f;
-            DownButtonVec_Z = 0f;
+            downButton = buttonUp;
+            downButtonVec_X = 0f;
+            downButtonVec_Z = 0f;
         }
 
 
         if (Input.GetKey(KeyCode.A))
         {
-            LeftButton = ButtonDown;
-            LeftButtonVec_X = -_Camera.transform.right.x;
-            LeftButtonVec_Z = -_Camera.transform.right.z;
+            leftButton = buttonDown;
+            leftButtonVec_X = -_Camera.transform.right.x;
+            leftButtonVec_Z = -_Camera.transform.right.z;
         }
         else if (Input.GetKeyUp(KeyCode.A))
         {
-            LeftButton = ButtonUp;
-            LeftButtonVec_X = 0f;
-            LeftButtonVec_Z = 0f;
+            leftButton = buttonUp;
+            leftButtonVec_X = 0f;
+            leftButtonVec_Z = 0f;
         }
 
 
         if (Input.GetKey(KeyCode.D))
         {
-            RightButton = ButtonDown;
-            RightButtonVec_X = _Camera.transform.right.x;
-            RightButtonVec_Z = _Camera.transform.right.z;
+            rightButton = buttonDown;
+            rightButtonVec_X = _Camera.transform.right.x;
+            rightButtonVec_Z = _Camera.transform.right.z;
         }
         else if (Input.GetKeyUp(KeyCode.D))
         {
-            RightButton = ButtonUp;
-            RightButtonVec_X = 0f;
-            RightButtonVec_Z = 0f;
+            rightButton = buttonUp;
+            rightButtonVec_X = 0f;
+            rightButtonVec_Z = 0f;
         }
 
         //총 Prees된 개수
-        PushCount = UpButton + DownButton + LeftButton + RightButton;
+        pushCount = upButton + downButton + leftButton + rightButton;
 
         //입력이 됐다면
-        if (PushCount > 0)
+        if (battle == false)
         {
-            if (Run == true)
+            if (pushCount > 0)
             {
-                MoveSpeed = 6f;
+                if (run == true)
+                {
+                    moveSpeed = 8f;
+                    runing = true;
+                }
+                else
+                {
+                    moveSpeed = 4f;
+                    runing = false;
+                }
+
+                //버튼이 눌린 값을 모두 더해 Vector의 평균값을 구함
+                forwardVec_X = (upButtonVec_X + downButtonVec_X + leftButtonVec_X + rightButtonVec_X) / pushCount;
+                forwardVec_Z = (upButtonVec_Z + downButtonVec_Z + leftButtonVec_Z + rightButtonVec_Z) / pushCount;
+
+                //forward가 될 Vector값
+                forwardVec = new Vector3(forwardVec_X, 0f, forwardVec_Z);
+
+                //천천히 Forward값을 변경
+                this.transform.forward = Vector3.Lerp(this.transform.forward, forwardVec, 0.5f);
+
+                //실제 움직임값
+
+                this.transform.Translate(this.transform.forward * 0.005f * moveSpeed, Space.World);
+
+
+
+                _Ani.SetBool("Run", run);
+                _Ani.SetBool("Walk", true);
             }
             else
             {
-                MoveSpeed = 3f;
+                forwardVec_X = 0f;
+                forwardVec_Z = 0f;
+
+                _Ani.SetBool("Run", false);
+                _Ani.SetBool("Walk", false);
+                runing = false;
+
             }
-
-            //버튼이 눌린 값을 모두 더해 Vector의 평균값을 구함
-            ForwardVec_X = (UpButtonVec_X + DownButtonVec_X + LeftButtonVec_X + RightButtonVec_X) / PushCount;
-            ForwardVec_Z = (UpButtonVec_Z + DownButtonVec_Z + LeftButtonVec_Z + RightButtonVec_Z) / PushCount;
-
-            //forward가 될 Vector값
-            ForwardVec = new Vector3(ForwardVec_X, 0f, ForwardVec_Z);
-
-            //천천히 Forward값을 변경
-            this.transform.forward = Vector3.Lerp(this.transform.forward, ForwardVec, 0.5f);
-
-            //실제 움직임값
-
-            this.transform.Translate(this.transform.forward * 0.005f * MoveSpeed, Space.World);
-
-
-
-            Ani.SetBool("Run", Run);
-            Ani.SetBool("Walk", true);
-        }
-        else
-        {
-            ForwardVec_X = 0f;
-            ForwardVec_Z = 0f;
-
-            Ani.SetBool("Run", false);
-            Ani.SetBool("Walk", false);
-           
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            Run = true;
+            if (_UIMgr.Stamina_Gauge <= 0f)
+            {
+                run = false;
+            }
+            else
+            {
+                run = true;
+            }
         }
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            Run = false;
+            run = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if(battle == false)
+            {
+                battle = true;
+                _Ani.SetBool("Battle", battle);
+                WeaponSpeed();
+            }
+            
+            attack = true;
+            idleComebackCheck = true;
+           
+           
+           
+            if(comboTime == true || wait == true)
+            {
+                comboTime = false;
+                _Ani.SetBool("Attack_Basic" + attack_Combo, attack);
+                wait = false;
+                _Ani.SetBool("Wait", wait);
+            }
+        }
+
+
+    }
+
+    //장착한 무기의 스피드를 확인 (공격 시작전 한번만 확인하면됌)
+    void WeaponSpeed()
+    {
+        for (int i = 0; i < _Weapon.transform.childCount; i++)
+        {
+            if (_Weapon.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                weapon_kind = _Weapon.transform.GetChild(i).gameObject.tag;
+
+                if (weapon_kind == "Axe")
+                {
+                    weapon_Speed = 0.8f;
+                }
+                else if (weapon_kind == "TwoHand_Sword")
+                {
+                    weapon_Speed = 1f;
+                }
+                else if (weapon_kind == "OneHand_Sword")
+                {
+                    weapon_Speed = 1.2f;
+                }
+
+
+                _Ani.SetFloat("Weapon_Speed", weapon_Speed);
+                return;
+            }
         }
     }
 
-    
+    //공격후 다음공격을 확인하기위한 준비 이벤트 (Attack)
+    void Event_Attack_NextPreparations()
+    {
+        StopCoroutine(timer_AttackCombo);
+        StopCoroutine(timer_BattleState_Off);
+        comboTime = false;
+        attack = false;
+        _Ani.SetBool("Attack_Basic" + attack_Combo, attack);
+
+
+        attack_Combo += 1;
+
+        if (attack_Combo >= 3)
+        {
+            attack_Combo = 0;
+        }
+        wait = false;
+        _Ani.SetBool("Wait", wait);
+    }
+
+    //attack도중 combo를 이어서 할수있는지 확인해주는 이벤트(attack)
+    void Event_AttackCombo()
+    {
+        comboTime = true;
+
+    }
+
+    //아무런 공격입력이 없을시 wait상태로 (attack);
+    void Event_AttackWait()
+    {
+
+        combo = true;
+        wait = true;
+        _Ani.SetBool("Wait", wait);
+
+    }
+
+
+    //Wait 애니메이션 진입시 다음 콤보를 이어가도록함 (Wait -> attack)
+    void Event_BattleCombo()
+    {
+        if (combo == true)
+        {
+            StartCoroutine(timer_AttackCombo);
+        }
+    }
+
+    IEnumerator Timer_AttackCombo()
+    {
+        
+        combo = false;
+        yield return new WaitForSecondsRealtime(4f);
+        attack_Combo = 0;
+    }
+
+    //Wait 애니메이션 진입시 한번만 돌아가도록함 (WAIT -> IDLE)
+    void Event_BattleState_Off()
+    {
+        if (idleComebackCheck == true)
+        {
+            StartCoroutine(timer_BattleState_Off);
+        }
+    }
+
+    IEnumerator Timer_BattleState_Off()
+    {
+        idleComebackCheck = false;
+        
+        yield return new WaitForSecondsRealtime(8f);
+        wait = false;
+        _Ani.SetBool("Wait", wait);
+        battle = false;
+        _Ani.SetBool("Battle", battle);
+    }
+
+
+
+
 }
